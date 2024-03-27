840463982e2adff6ff100e0104ab25058fc2241a
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2024-03-27 16:56:21 +0100

    on the way to new paper impl

--- experiments/volume_integration.py
+++ experiments/volume_integration.py
@@ -3,17 +3,22 @@ import scipy.special
 import matplotlib.pyplot as plt
 import math
 
-# centroids = np.array([0.4, 2.4, 2.9, 3.5, 5.2, 7.5, 8.4, 9.3]).reshape(-1, 1)
-# SDs =       np.array([0.4, 0.5, 0.9, 0.7, 0.2, 0.5, 0.8, 1.3]).reshape(-1, 1)
-# weights =   np.array([0.4, 0.5, 0.9, 0.7, 8.2, 0.5, 1.8, 0.5]).reshape(-1, 1) * 0.2
-centroids = np.array([5.4, ]).reshape(-1, 1)
-SDs =       np.array([0.8, ]).reshape(-1, 1)
-weights =   np.array([1.5, ]).reshape(-1, 1)
+centroids = np.array([0.4, 2.4, 2.9, 3.5, 5.2, 7.5, 8.4, 9.3]).reshape(-1, 1)
+SDs =       np.array([0.4, 0.5, 0.9, 0.7, 0.2, 0.5, 0.8, 1.3]).reshape(-1, 1)
+weights =   np.array([0.4, 0.5, 0.9, 0.7, 1.2, 0.5, 1.8, 0.5]).reshape(-1, 1) * 0.5
+c_g =       np.array([0.5, 0.2, 0.4, 0.1, 0.2, 0.8, 0.8, 0.3]).reshape(-1, 1)
+
+# centroids = np.array([1.4, 7.4, 7.4, ]).reshape(-1, 1)
+# SDs =       np.array([0.8, 0.8, 0.8, ]).reshape(-1, 1)
+# weights =   np.array([1.5, 0.5, 0.5, ]).reshape(-1, 1)
+# c_g =       np.array([0.2, 0.9, 0.1, ]).reshape(-1, 1)
+
+def g(xes: np.array) -> np.array:
+    norm_factor = 1 / (SDs * math.sqrt(2 * math.pi))
+    return weights * norm_factor * np.exp(-0.5 * (xes - centroids)*(xes - centroids) / (SDs * SDs))
 
 def gm(xes: np.array) -> np.array:
-    norm_factor = 1 / (SDs * math.sqrt(2 * math.pi))
-    g_evals = weights * norm_factor * np.exp(-0.5 * (xes - centroids)*(xes - centroids) / (SDs * SDs))
-    return np.sum(g_evals, axis=0)
+    return np.sum(g(xes), axis=0)
 
 
 def gm_cdf(xes: np.array) -> np.array:
@@ -22,9 +27,9 @@ def gm_cdf(xes: np.array) -> np.array:
 
 def integrate_gm_0_to(xes: np.array) -> np.array:
     return gm_cdf(xes) - gm_cdf(np.zeros_like(xes))
-    # scaling_factor = 1 / (SDs * math.sqrt(2))
-    # return np.sum(weights * 0.5 * (scipy.special.erf((xes - centroids) * scaling_factor) - scipy.special.erf((-centroids) * scaling_factor)), axis=0)
 
+def c(xes: np.array) -> np.array:
+    return np.sum(c_g * g(xes)/gm(xes), axis=0)
 
 delta = 0.01
 xes = np.arange(0, 10, delta, dtype=np.float64).reshape(1, -1)
@@ -40,6 +45,7 @@ plt.plot(xes.reshape(-1), gm_int, label='gm integrated')
 plt.plot(xes.reshape(-1), integrate_gm_0_to(xes), label='gm integrated 2')
 plt.plot(xes.reshape(-1), np.exp(-1 * gm_int), label='transmittance')
 plt.plot(xes.reshape(-1), gm_evals * np.exp(-1 * gm_int), label='contribution')
+plt.plot(xes.reshape(-1), c(xes), label='c')
 plt.legend()
 plt.show()
 print("")
